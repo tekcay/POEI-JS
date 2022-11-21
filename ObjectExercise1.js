f104927ba234839ac92d7ca0e4e3bffa01a3a99c
tekcay <pierreblc@hotmail.fr>
2022-11-21 15:29:43 +0100

    tried to work around to get both default and custom constructors, but does not work

--- ObjectExercise1.js
+++ ObjectExercise1.js
@@ -1,11 +1,21 @@
 class Village {
-    constructor(nbVillageois, nbBois, nbPierre, nbOr, Bâtiments) {
-        this.nbVillageois = nbVillageois;
-        this.nbBois = nbBois;
-        this.nbPierre = nbPierre;
-        this.nbOr = nbOr;
-        this.Bâtiments = Bâtiments;
+    constructor() {
+        this.nbVillageois = 1;
+        this.nbBois = 100;
+        this.nbPierre = 100;
+        this.nbOr = 0;
+        this.Bâtiments = ["HDV"];
     }
+
+    //custom
+    static CustomVillage(nbVillageois, nbBois, nbPierre, nbOr, Bâtiments) {
+        return new Village(nbVillageois, nbBois, nbPierre, nbOr, Bâtiments)
+    }
+
+    static DefaultVillage() {
+        return new Village(1, 100, 100, 0, ["HDV"]);
+    }
+
     getNbVillageois() {return this.nbVillageois};
     getNbBois() {return this.nbBois};
     getNbPierre() {return this.nbPierre};
@@ -16,6 +26,8 @@ class Village {
 
 display = a => console.log(a);
 
-let village = new Village(1, 100, 100, 0, ["HDV"]);
+let village2 = new Village();
+let village = new Village(1, 200, 300, 0, ["HDV"]);
 
+display(village2);
 display(village);
