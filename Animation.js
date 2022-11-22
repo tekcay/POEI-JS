21f62556550404f9385d2a0ef7f954b7268d00fa
tekcay <pierreblc@hotmail.fr>
2022-11-22 09:18:17 +0100

    built the main structure of the class

---
+++ Animation.js
@@ -0,0 +1,41 @@
+/*
+Créer un module Animation.js qui contiendra la classe Animation qui aura les méthodes suivantes:
+- constructor(element)
+- deplacer(x, y)
+- agrandir(largeur, hauteur)
+- changerCouleur(couleur)
+- pivoter(degrees)
+- disparaitre()
+- apparaitre()
+*/
+
+
+class Animation {
+    constructor(element) {
+        this.element = element;
+    }
+
+    deplacer(x, y) {
+
+    }
+
+    agrandir(largeur, hauteur) {
+
+    }
+
+    changerCouleur(couleur) {
+
+    }
+
+    pivoter(degrees) {
+
+    }
+
+    disparaitre() {
+
+    }
+
+    apparaitre() {
+        
+    }
+}
